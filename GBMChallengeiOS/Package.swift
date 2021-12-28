920a7155e6f039237a54868832a21a342b411157
Aldo Fuentes <aukfuentesm@gbm.com.mx>
2021-12-28 17:41:47 -0600

    Moved ConstraintHelpers to swift package.

--- GBMChallengeiOS/Package.swift
+++ GBMChallengeiOS/Package.swift
@@ -17,13 +17,13 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         
         .package(url: "https://github.com/danielgindi/Charts.git", .upToNextMajor(from: "4.0.1")),
+        .package(name: "ConstraintHelpers", path: "../ConstraintHelpers"),
         .package(name: "GBMChallengeKit", path: "../GBMChallengeKit"),
         .package(name: "CombineHelpers", path: "../CombineHelpers"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "ConstraintHelpers", path: "./Sources/ConstraintHelpers.xcframework"),
         .target(
             name: "GBMChallengeiOS",
             dependencies: ["GBMChallengeKit", "ConstraintHelpers", "Charts", "CombineHelpers"],
